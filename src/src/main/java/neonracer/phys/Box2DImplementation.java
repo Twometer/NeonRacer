6969f65767b5d9e6e041d23a84ea4b6aee5983e9
QXTR <45564162+QXTR@users.noreply.github.com>
2019-04-05 22:10:16 +0200

    Box2D gravity calculation demonstration

--- src/src/main/java/neonracer/phys/Box2DImplementation.java
+++ src/src/main/java/neonracer/phys/Box2DImplementation.java
@@ -1,4 +1,64 @@
 package neonracer.phys;
 
+import org.jbox2d.common.*;
+import org.jbox2d.dynamics.*;
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.joints.JointDef;
+import org.jbox2d.collision.shapes.*;
+import org.joml.Vector2f;
+
 public class Box2DImplementation {
+
+    private World world;
+
+    private BodyDef bd;
+
+    private Body body;
+
+    private PolygonShape ps;
+
+    private FixtureDef fd;
+
+    public Box2DImplementation()
+    {
+        world = new World(new Vec2(0,1));
+        world.setWarmStarting(true);
+        world.setContinuousPhysics(true);
+        bd = new BodyDef();
+        bd.type = BodyType.DYNAMIC;
+        bd.position.set(0,0);
+        ps = new PolygonShape();
+        ps.setAsBox(1,1);
+        fd = new FixtureDef();
+        fd.shape = ps;
+    }
+
+    public void createEntity()
+    {
+        body = world.createBody(bd);
+        body.createFixture(fd);
+    }
+
+    public void step(float timeStep, int i, int i1) {
+        world.step(timeStep,i,i1);
+        world.clearForces();
+    }
+
+    public Vector2f Vec2toVector2f(Vec2 v)
+    {
+        Vector2f vf = new Vector2f(v.x,v.y);
+        return vf;
+    }
+
+    public Vec2 Vector2f2toVec2(Vector2f vf)
+    {
+        Vec2 v = new Vec2(vf.x,vf.y);
+        return v;
+    }
+
+    public Vector2f currentPosition()
+    {
+        Transform xf = body.getTransform();
+        return Vec2toVector2f(xf.p);
+    }
 }
--- src/src/main/java/neonracer/phys/PhysicsEngine.java
+++ src/src/main/java/neonracer/phys/PhysicsEngine.java
@@ -10,6 +10,8 @@ public class PhysicsEngine {
 
     private GameContext gameContext;
 
+    private Box2DImplementation box2DImplementation;
+
     private float x = 0.01f;
     private float y = 0.005f;
     private Vector2f v = new Vector2f(x,y);;
@@ -18,13 +20,16 @@ public class PhysicsEngine {
 
     public void initialize(GameContext gameContext) {
         this.gameContext = gameContext;
+        box2DImplementation = new Box2DImplementation();
+        box2DImplementation.createEntity();
     }
 
     public void onTick() {
         GameState gameState = gameContext.getGameState();
         EntityCar playerEntity = gameState.getPlayerEntity();
-        playerEntity.setPosition(updatePosition(playerEntity.getPosition()));
+        playerEntity.setPosition(box2DImplementation.currentPosition());//updatePosition(playerEntity.getPosition()));
         playerEntity.setRotation(updateRotation(playerEntity.getRotation()));
+        box2DImplementation.step(1.0f / 60f,8,10);
     }
 
     private Vector2f updatePosition(Vector2f curr)
