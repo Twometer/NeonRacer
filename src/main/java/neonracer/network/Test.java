22ee397e190fabad12b8d6ff8e903f603fdb7b87
Daniel Lerch <daniel.p.lerch@gmail.com>
2019-05-04 22:36:20 +0200

    Add entity management to protocol and align README

--- src/main/java/neonracer/network/Test.java
+++ src/main/java/neonracer/network/Test.java
@@ -1,11 +1,16 @@
 package neonracer.network;
 
+import neonracer.network.proto.Entity;
 import neonracer.network.proto.Login;
 
 public class Test {
 
     void test() {
         Login.Request.newBuilder().setNickname("test").build();
-        Login.Response.newBuilder().setStatus(Login.Status.SUCCESS);
+        Login.Response.newBuilder().setStatus(Login.Status.SUCCESS).build();
+
+        Entity.Create.newBuilder().setEntityId(0).build();
+        Entity.Update.newBuilder().setEntityId(0).setPositionX(5).setVelocityX(0.1f).build();
+        Entity.Delete.newBuilder().setEntityId(0).build();
     }
 }
