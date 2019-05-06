095815a5461218672c91eee779175b0275eb0146
Daniel Lerch <daniel.p.lerch@gmail.com>
2019-05-06 14:00:39 +0200

    Add race/lobby management protocol

--- src/main/java/neonracer/network/Test.java
+++ src/main/java/neonracer/network/Test.java
@@ -2,15 +2,20 @@ package neonracer.network;
 
 import neonracer.network.proto.Entity;
 import neonracer.network.proto.Login;
+import neonracer.network.proto.Race;
 
 public class Test {
 
     void test() {
-        Login.Request.newBuilder().setNickname("test").build();
-        Login.Response.newBuilder().setStatus(Login.Status.SUCCESS).build();
+        Login.LoginRequest.newBuilder().setNickname("test").build();
+        Login.LoginResponse.newBuilder().setStatus(Login.LoginStatus.SUCCESS).build();
 
         Entity.Create.newBuilder().setEntityId(0).build();
-        Entity.Update.newBuilder().setEntityId(0).setPositionX(5).setVelocityX(0.1f).build();
+        Entity.Update.newBuilder().setEntityId(0).setX(5).setVelocity(0.1f).build();
         Entity.Delete.newBuilder().setEntityId(0).build();
+
+        Race.Prepare.newBuilder().setRemainingMilliseconds(10000).build();
+        Race.Join.newBuilder().build();
+        Race.Start.newBuilder().setElapsedMilliseconds(-5000).setLapCount(3).build();
     }
 }
