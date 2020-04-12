537f35cd2ea2c5bf70adf75f52ed6375b6d356ad
Twometer <twometer@outlook.de>
2020-04-12 12:45:54 +0200

    Enable mipmapping to reduce eye-hurting artifacts during driving

--- src/main/java/neonracer/render/gl/GlLoader.java
+++ src/main/java/neonracer/render/gl/GlLoader.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 
 import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.glGenerateMipmap;
 
 /**
  * This class handles loading of resources to OpenGL,
@@ -102,10 +103,12 @@ public class GlLoader {
             int textureId = glGenTextures();
             glBindTexture(GL_TEXTURE_2D, textureId);
             glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.getWidth(), image.getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+            glGenerateMipmap(GL_TEXTURE_2D);
+
             glBindTexture(GL_TEXTURE_2D, 0);
 
             return new Texture(textureId, image.getWidth(), image.getHeight());
